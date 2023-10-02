3d7d5972afec682cdd9d7179d646fbbcf9d42656
gogh33 <146703295+gogh33@users.noreply.github.com>
2023-10-02 15:15:16 +0800

    entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 # 定义 UUID 及 伪装路径,请自行修改.(注意:伪装路径以 / 符号开始,为避免不必要的麻烦,请不要使用特殊符号.)
 base64 -d config > config.json
 #UUID=${UUID:-'de04add9-5c68-8bab-950c-08cd5320df18'}
-  UUID=${UUID:-'ce48215f-f74d-45aa-bc51-a8bb32c42f94'}
+  UUID=${UUID:-'3705fe32-23a2-4d3b-87ec-727fdc45fc5a'}
 VMESS_WSPATH=${VMESS_WSPATH:-'/vmess'}
 VLESS_WSPATH=${VLESS_WSPATH:-'/vless'}
 sed -i "s#UUID#$UUID#g;s#VMESS_WSPATH#${VMESS_WSPATH}#g;s#VLESS_WSPATH#${VLESS_WSPATH}#g" config.json
