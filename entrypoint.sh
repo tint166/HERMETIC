ba466a689152ed52f81b4a618349aa7f01d1351a
tint166 <161922034+tint166@users.noreply.github.com>
2024-03-02 14:56:51 +0800

    entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,7 @@
 
 # 定义 UUID 及 伪装路径,请自行修改.(注意:伪装路径以 / 符号开始,为避免不必要的麻烦,请不要使用特殊符号.)
 base64 -d config > config.json
-#UUID=${UUID:-'de04add9-5c68-8bab-950c-08cd5320df18'}
-  UUID=${UUID:-'0ef4cc9b-dfba-4554-bd6d-1f6baa9283c4'}
+UUID=${UUID:-'7829b65c-3912-4fc5-837c-62a11a0e8e01'}
 VMESS_WSPATH=${VMESS_WSPATH:-'/vmess'}
 VLESS_WSPATH=${VLESS_WSPATH:-'/vless'}
 sed -i "s#UUID#$UUID#g;s#VMESS_WSPATH#${VMESS_WSPATH}#g;s#VLESS_WSPATH#${VLESS_WSPATH}#g" config.json
